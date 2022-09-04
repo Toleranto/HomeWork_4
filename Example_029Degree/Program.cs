c0a7e8609bc93cd7a60a89b6d6bdd9b4bc5b3746
Eugenius <eugenius8447@gmail.com>
2022-09-05 01:31:11 +0300

    Degree

---
+++ Example_029Degree/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и
+// возводит число A в натуральную степень B.
+
+Console.Clear();
+Console.WriteLine("Давайте возведем число в степень!!!");
+Console.WriteLine("Введите число: ");
+int basis = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите степень в которую возводить число: ");
+int degree = Convert.ToInt32 (Console.ReadLine());
+Math.Pow(basis, degree);
+Console.WriteLine($"{basis} в степени {degree}: {Math.Pow(basis, degree)}");
