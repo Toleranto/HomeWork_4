0a5ae112d5ccdb8ef3967eb73bd8993a5be048b5
Eugenius <eugenius8447@gmail.com>
2022-09-05 01:32:07 +0300

    Array

---
+++ Example_029Array/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+
+// Напишите программу, которая задаёт массив из 8 элементов и выводит 
+//их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+Console.Clear();
+int[] collection = new int[8];
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0,50);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length - 1;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + ",");
+        position++;
+    }
+}
+
+
+FillArray(collection);
+Console.Write("[");
+PrintArray(collection);
+Console.Write($"{(collection[7])}]");
+
+
