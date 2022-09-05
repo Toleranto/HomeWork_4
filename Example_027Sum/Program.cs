14143502d642fd13fcc3e71eac563162a4ce548e
Eugenius <eugenius8447@gmail.com>
2022-09-05 12:07:52 +0300

    Example_027Sum

--- Example_027Sum/Program.cs
+++ Example_027Sum/Program.cs
@@ -1,10 +1,22 @@
-﻿// Напишите программу, которая принимает на вход число и выдаёт 
-// сумму цифр в числе.
-// 452 -> 11
-// 82 -> 10
-// 9012 -> 12
-Console.Clear();
-Console.WriteLine("Введите число: ");
-int Digits = Convert.ToInt32(Console.ReadLine());
-int Sum = int Digits / 100 + (int Digits/10) % 10 + int Digits % 10;
-Console.WriteLine($" Сумма равна = {Sum}");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Напишите программу, которая принимает на вход число и выдаёт 
+        // сумму цифр в числе.
+        // 452 -> 11
+        // 82 -> 10
+        // 9012 -> 12
+        Console.Clear();
+        Console.WriteLine("Введите число: ");
+        int Digits = Convert.ToInt32(Console.ReadLine());
+        int Sum = 0;
+        while (Digits > 0)
+        {
+            Sum = Sum + Digits % 10;
+            Digits = Digits / 10;
+        }
+
+        Console.WriteLine($"Сумма цифр числа равна = {Sum}");
+    }
+}
